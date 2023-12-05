219716720a0c7af3efa8977bcbaa9c18095154a6
Juanvi <juanvi1921.cursos@gmail.com>
2023-12-05 12:44:46 +0100

    Subida de cambios al repositorio

---
+++ mostrarNumeos/src/mostrarNumeros.java
@@ -0,0 +1,40 @@
+
+import java.util.Scanner;
+
+
+
+public class mostrarNumeros {
+
+
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+
+        System.out.println("Por favor, ingresa el primer número:");
+
+        double numero1 = scanner.nextDouble();
+
+
+
+        System.out.println("Ahora, ingresa el segundo número:");
+
+        double numero2 = scanner.nextDouble();
+
+
+
+        System.out.println("Los números ingresados son:");
+
+        System.out.println("Número 1: " + numero1);
+
+        System.out.println("Número 2: " + numero2);
+
+
+
+        scanner.close();
+
+    }
+
+}
